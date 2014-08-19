6a73ab4a793fd1f2d6a880dddd1a83802b6a3e1c
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-19 11:27:03 +1000

    Simple ice candidate validation completed

--- candidate.js
+++ candidate.js
@@ -1,12 +1,83 @@
+var rePrefix = /^(?:a=)?candidate:/;
+var reIP = /^(\d+\.){3}\d+$/;
+
+/*
+
+validation rules as per:
+http://tools.ietf.org/html/draft-ietf-mmusic-ice-sip-sdp-03#section-8.1
+
+   candidate-attribute   = "candidate" ":" foundation SP component-id SP
+                           transport SP
+                           priority SP
+                           connection-address SP     ;from RFC 4566
+                           port         ;port from RFC 4566
+                           SP cand-type
+                           [SP rel-addr]
+                           [SP rel-port]
+                           *(SP extension-att-name SP
+                                extension-att-value)
+
+   foundation            = 1*32ice-char
+   component-id          = 1*5DIGIT
+   transport             = "UDP" / transport-extension
+   transport-extension   = token              ; from RFC 3261
+   priority              = 1*10DIGIT
+   cand-type             = "typ" SP candidate-types
+   candidate-types       = "host" / "srflx" / "prflx" / "relay" / token
+   rel-addr              = "raddr" SP connection-address
+   rel-port              = "rport" SP port
+   extension-att-name    = token
+   extension-att-value   = *VCHAR
+   ice-char              = ALPHA / DIGIT / "+" / "/"
+*/
+var partValidation = [
+  [ /.+/, 'invalid foundation component' ],
+  [ /\d+/, 'invalid component id' ],
+  [ /(UDP|TCP)/i, 'transport must be TCP or UDP' ],
+  [ /\d+/, 'numeric priority expected' ],
+  [ reIP, 'invalid connection address' ],
+  [ /\d+/, 'invalid connection port' ],
+  [ /typ/, 'Expected "typ" identifier' ],
+  [ /.+/, 'Invalid candidate type specified' ]
+];
+
 /**
   ### `rtc-validator/candidate`
 
   Validate that an `RTCIceCandidate` (or plain old object with data, sdpMid,
   etc attributes) is a valid ice candidate.
 
+  Specs reviewed as part of the validation implementation:
+
+  - <http://tools.ietf.org/html/draft-ietf-mmusic-ice-sip-sdp-03#section-8.1>
+  - <http://tools.ietf.org/html/rfc5245>
+
 **/
 module.exports = function(data) {
   var errors = [];
+  var candidate = data && data.candidate;
+  var prefixMatch = candidate && rePrefix.exec(candidate);
+  var parts = prefixMatch && candidate.slice(prefixMatch[0].length).split(/\s/);
+
+  if (! candidate) {
+    return [ new Error('empty candidate') ];
+  }
+
+  // check that the prefix matches expected
+  if (! prefixMatch) {
+    return [ new Error('candidate did not match expected sdp line format') ];
+  }
+
+  // perform the part validation
+  errors = errors.concat(parts.map(validateParts)).filter(Boolean);
 
   return errors;
 };
+
+function validateParts(part, idx) {
+  var validator = partValidation[idx];
+
+  if (validator && (! validator[0].test(part))) {
+    return new Error(validator[1]);
+  }
+}
--- test/candidate.js
+++ test/candidate.js
@@ -1,1 +1,27 @@
 var test = require('tape');
+var validate = require('../candidate');
+
+var validCandidates = [
+  { mid: 'data', candidate: 'a=candidate:2441410931 1 udp 2121670399 172.17.42.1 40992 typ host generation 0' }
+];
+var invalidCandidates = [
+  { mid: 'data', candidate: 'a=candidate:2365396244 1 udp 1853300479 10.17.131.21 59229 typ  raddr 172.17.42.1 rport 59229 generation 0' }
+];
+
+test('valid candidates pass validation', function(t) {
+  t.plan(validCandidates.length);
+
+  validCandidates.forEach(function(candidate) {
+    t.equal(validate(candidate).length, 0, 'validated ok');
+  });
+});
+
+test('invalid candidates are detected', function(t) {
+  t.plan(invalidCandidates.length);
+
+  invalidCandidates.forEach(function(candidate) {
+    var errors = validate(candidate);
+
+    t.ok(errors.length > 0, 'validation error detected');
+  });
+});
