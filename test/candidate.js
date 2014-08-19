d996425fe10e847cba920bf064c3714223a6300c
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-19 16:22:34 +1000

    Add raw string tests to validation tests

--- test/candidate.js
+++ test/candidate.js
@@ -2,10 +2,12 @@ var test = require('tape');
 var validate = require('../candidate');
 
 var validCandidates = [
-  { mid: 'data', candidate: 'a=candidate:2441410931 1 udp 2121670399 172.17.42.1 40992 typ host generation 0' }
+  { mid: 'data', candidate: 'a=candidate:2441410931 1 udp 2121670399 172.17.42.1 40992 typ host generation 0' },
+  'a=candidate:2441410931 1 udp 2121670399 172.17.42.1 40992 typ host generation 0'
 ];
 var invalidCandidates = [
-  { mid: 'data', candidate: 'a=candidate:2365396244 1 udp 1853300479 10.17.131.21 59229 typ  raddr 172.17.42.1 rport 59229 generation 0' }
+  { mid: 'data', candidate: 'a=candidate:2365396244 1 udp 1853300479 10.17.131.21 59229 typ  raddr 172.17.42.1 rport 59229 generation 0' },
+  'a=candidate:2365396244 1 udp 1853300479 10.17.131.21 59229 typ  raddr 172.17.42.1 rport 59229 generation 0'
 ];
 
 test('valid candidates pass validation', function(t) {
